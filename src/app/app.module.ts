4c59b73e888c7b5ada419214c91cf6306769db5a
subcero123 <subcero139@hotmail.com>
2024-04-04 18:52:55 -0600

    update front

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PrincipalScreenComponent } from './screens/principal-screen/principal-screen.component';
 import { HeaderComponent } from './components/header/header.component';
+import { ExperienciaLaboralComponent } from './componenets/experiencia-laboral/experiencia-laboral.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PrincipalScreenComponent,
-    HeaderComponent
+    HeaderComponent,
+    ExperienciaLaboralComponent
   ],
   imports: [
     BrowserModule,
--- src/app/screens/principal-screen/principal-screen.component.ts
+++ src/app/screens/principal-screen/principal-screen.component.ts
@@ -1,26 +1,59 @@
-import { Component, HostListener, Renderer2 } from '@angular/core';
+import { Component, HostListener, Renderer2} from '@angular/core';
+import { trigger, transition, style, animate } from '@angular/animations';
+
 
 @Component({
   selector: 'app-principal-screen',
   templateUrl: './principal-screen.component.html',
   styleUrls: ['./principal-screen.component.scss'],
+  animations: [
+    trigger('scrollAnimation', [
+      transition(':enter', [
+        style({ transform: 'translateY(-100%)' }),
+        animate('0.5s', style({ transform: 'translateY(0)' }))
+      ]),
+      transition(':leave', [
+        style({ transform: 'translateY(0)' }),
+        animate('0.5s', style({ transform: 'translateY(100%)' }))
+      ])
+    ])
+  ]
 })
 export class PrincipalScreenComponent {
 
+  showSlide1Content: boolean = true;
   lightStyle: any = {};
+  progressBarWidth: number = 0;
+
+  constructor(private renderer: Renderer2) {
+      window.addEventListener('scroll', this.onWindowScroll.bind(this));
+   }
+
+
 
 
   @HostListener('document:mousemove', ['$event'])
   onMouseMove(event: MouseEvent) {
     const offsetX = event.clientX;
     const offsetY = event.clientY;
-    const brightness = 0.2; // Valor de brillo (ajustable según necesidades)
+    const brightness = 0.1; // Valor de brillo (ajustable según necesidades)
 
     this.lightStyle = {
-      top: offsetY - 150 + 'px',
-      left: offsetX - 150 + 'px',
+      top: offsetY - 100 + 'px',
+      left: offsetX - 100 + 'px',
       background: `radial-gradient(circle, rgba(255, 255, 255, ${brightness}), transparent)`
     };
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const scrollPosition = window.pageYOffset;
+    const totalHeight = document.body.scrollHeight - window.innerHeight;
+    this.progressBarWidth = (scrollPosition / totalHeight) * 100;
+
+
+
+    this.progressBarWidth = (scrollPosition / totalHeight) * 100;
   }
   
 }
